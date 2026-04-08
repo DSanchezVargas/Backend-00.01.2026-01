ef2449d8942477e43bf737895fc80cd813922ae9
richard-asto <SV09681015@idat.pe>
2026-04-08 00:24:33 -0500

    Correccion server.js

--- Hackaton11/SV09681015/server.js
+++ Hackaton11/SV09681015/server.js
@@ -1,6 +1,5 @@
 // Forzar DNS de Google en Node.js
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 
 const express = require('express');
 const mongoose = require('mongoose');
